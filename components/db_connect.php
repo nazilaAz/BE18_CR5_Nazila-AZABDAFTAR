ac1e6d0a394805e0c220bd73c1f8ae34893d8f80
Nazila <azabdaftar@outlook.com>
2023-05-04 12:39:02 +0200

    change Db

--- components/db_connect.php
+++ components/db_connect.php
@@ -1,9 +1,13 @@
 <?php
 
-$localhost = "127.0.0.1";
-$username = "root";
-$password = "";
-$dbname = "be18_cr5_animal_adoption_nazila-azabdaftar";
+$localhost = "173.212.235.205";
+$username = "nazilacodefactor_adminuser";
+$password = "Ham19219414@Adminuser";
+$dbname = "nazilacodefactor_be18_cr5_animal_adoption_nazila-azabdaftar";
+// $localhost = "127.0.0.1";
+// $username = "root";
+// $password = "";
+// $dbname = "be18_cr5_animal_adoption_nazila-azabdaftar";
 
 // create connection
 $connect = new  mysqli($localhost, $username, $password, $dbname);
